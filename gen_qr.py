29cdbaa663b669a8ccb046522d56b404db2d90a3
Mario-dango <mario.dango@users.noreply.github.com>
2022-03-05 22:23:17 -0300

    Se mejoró el archivo generador de QR's

--- gen_qr.py
+++ gen_qr.py
@@ -31,31 +31,51 @@ else:
 # Obtenemos el parámetro -t (tipo)
 if args.tipo:
     tipoQR =  args.tipo
-else:
-    tipoQR = 'cuadrado_grande'
-tipoQR = tipoQR.upper()
+else:    
+    tipoQR = input("Introduzca la forma del código QR: \n\
+        1: círculo\n\
+        2: cuadrado\n\
+        3: barra vertical\n\
+        4: barra horizontal\n\
+        5: redondeado\n\
+        6: cuadrados grandes\n\
+        Recordar colocar el solamente el nombre que figura después de los dos puntos ':' \n\
+        Por defecto será cuadrados grandes.")
+
+    tipoQR = tipoQR.upper()
+    # Establecemos el tipo de QR según el indicado por parámetro -t
+    if tipoQR == 'CÍRCULO':
+        tipoQRC = CircleModuleDrawer()
+    elif tipoQR == 'CUADRADO':
+        tipoQRC = GappedSquareModuleDrawer()
+    elif tipoQR == 'BARRA_VERTICAL':
+        tipoQRC = VerticalBarsDrawer()
+    elif tipoQR == 'BARRA_HORIZONTAL':
+        tipoQRC = HorizontalBarsDrawer()
+    elif tipoQR == 'REDONDEADO':
+        tipoQRC = RoundedModuleDrawer()
+    elif tipoQR == 'CUADRADO_GRANDE':
+        tipoQRC = SquareModuleDrawer()
+    else:
+        tipoQRC = SquareModuleDrawer()    
+
 # Obtenemos el parámetro -i (fichero de imagen QR)
 if args.imagen:
     imagenQR = args.imagen
 else:
-    imagenQR = os.path.dirname(os.path.abspath(__file__)) + '\codigo_qr.png'
+    ruta = input("incgrese el nombre y/o ruta del archivo a generar\n\
+        Ejemplo: '\QRS\tipos_QR\cuadrado.png' \nRecordar que la dirección es la que aparece entre comillas simples. ")
+    #ruta = 'drogas.png'
+    if not ruta:
+        imagenQR = os.path.dirname(os.path.abspath(__file__)) + '\codigo_qr.png'
+    else:
+        imagenQR = os.path.dirname(os.path.abspath(__file__)) + ruta
+    
+    # imagenQR = os.path.dirname(os.path.abspath(__file__)) + '\codigo_qr.png'
 
 # Aplicamos el valor al objeto QR
 qr.add_data(valorQR)
 
-# Establecemos el tipo de QR según el indicado por parámetro -t
-if tipoQR == 'CÍRCULO':
-    tipoQRC = CircleModuleDrawer()
-elif tipoQR == 'CUADRADO':
-    tipoQRC = GappedSquareModuleDrawer()
-elif tipoQR == 'BARRA_VERTICAL':
-    tipoQRC = VerticalBarsDrawer()
-elif tipoQR == 'BARRA_HORIZONTAL':
-    tipoQRC = HorizontalBarsDrawer()
-elif tipoQR == 'REDONDEADO':
-    tipoQRC = RoundedModuleDrawer()
-elif tipoQR == 'CUADRADO_GRANDE':
-    tipoQRC = SquareModuleDrawer()
 
 # Generamos el código QR y lo almacenamos en el fichero de imagen PNG
 img = qr.make_image(image_factory=StyledPilImage, module_drawer=tipoQRC)
